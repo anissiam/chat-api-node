9a4faffb816d7e29632d5503bd86e069205d848b
anis <anis.siam@hotmail.com>
2025-09-08 08:38:35 +0300

    ff

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const databaseURL = process.env.DATABASE_URL;
 app.use(cors({
     origin: [process.env.ORIGIN],
     methods: ["GET", "POST", "PUT","PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials:true
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json())
 app.use(express.urlencoded())
